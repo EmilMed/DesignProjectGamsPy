cae29d3cd03d7b947fadfb9400dd94bcee165bb7
arkodxs <arko.a.das@gmail.com>
2025-10-14 22:48:04 +0100

    Formatting of imports + finished

--- gams_temp.py
+++ gams_temp.py
@@ -1,22 +1,42 @@
-from gamspy import Container, Set, Parameter, Variable, Equation, Model, Sum, Sense, Options
+from gamspy import (Container, Set, Parameter, Variable,
+                    Equation, Model, Sum, Sense, Options)
 
+# Define model container
 m = Container()
+
+# -------------------------------------------------------------------------------#
+#                                 || Sets ||
+# -------------------------------------------------------------------------------#
 
 i = Set(Container=m, name='comps', description="Involved chemical components")
 j = Set(Container=m, name='streams', description="Flow streams in process")
 
+
+# -------------------------------------------------------------------------------#
+#                              || Parameters ||
+# -------------------------------------------------------------------------------#
+
+# Needs to be defined with actual data
+
 v1 = Parameter(
-     Container=m,
-     name='v1',
-     domain=i,  # over components
-     description="Stochiometric coefficient \
-                  of component i in haber reactor"
+    Container=m,
+    name='v1',
+    domain=i,  # over components
+    description="Stoichiometric coefficient of component i in haber reactor"
 )
 
+# -------------------------------------------------------------------------------#
+#                              || Variables ||
+# -------------------------------------------------------------------------------#
+
 F = Variable(
-     Container=m,
-     name="F",
-     domain=[i, j],  # per comp & stream
-     type="Positive",
-     description="Molar flowrate of component i in stream j"
+    Container=m,
+    name="F",
+    domain=[i, j],  # per comp & stream
+    type="Positive",
+    description="Molar flowrate of component i in stream j"
 )
+
+# -------------------------------------------------------------------------------#
+#                              || Equations || 
+# -------------------------------------------------------------------------------#

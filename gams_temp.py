d720734fad05ad61503d4b682d0b5e4dd39d1d64
arkodxs <arko.a.das@gmail.com>
2025-10-14 23:53:53 +0100

    Filled 'comps' set - needs confirmation for component naming

--- gams_temp.py
+++ gams_temp.py
@@ -4,17 +4,32 @@ from gamspy import (Container, Set, Parameter, Variable,
 # Define model container
 m = Container()
 
-# -------------------------------------------------------------------------------#
+# ===============================================================================#
 #                                 || Sets ||
-# -------------------------------------------------------------------------------#
+# ===============================================================================#
 
-i = Set(Container=m, name='comps', description="Involved chemical components")
+# Needs to be defined with actual data
+
+i = Set(
+    Container=m,
+    name='comps',
+    records=[
+        'H',
+        'H2O',
+        'O',
+        'N',
+        'Ar',
+        'CO2',
+    ],
+    description="Involved chemical components"
+)
+
 j = Set(Container=m, name='streams', description="Flow streams in process")
 
 
-# -------------------------------------------------------------------------------#
+# ===============================================================================#
 #                              || Parameters ||
-# -------------------------------------------------------------------------------#
+# ===============================================================================#
 
 # Needs to be defined with actual data
 
@@ -25,9 +40,15 @@ v1 = Parameter(
     description="Stoichiometric coefficient of component i in haber reactor"
 )
 
-# -------------------------------------------------------------------------------#
+X_ure = Parameter(
+    Container=m,
+    name='X_Ure',
+    description="Split fraction of ammonia from transport to urea process"
+)
+
+# ===============================================================================#
 #                              || Variables ||
-# -------------------------------------------------------------------------------#
+# ===============================================================================#
 
 F = Variable(
     Container=m,
@@ -37,6 +58,15 @@ F = Variable(
     description="Molar flowrate of component i in stream j"
 )
 
-# -------------------------------------------------------------------------------#
-#                              || Equations || 
-# -------------------------------------------------------------------------------#
+
+X_haber = Parameter(
+    Container=m,
+    name='X_haber',
+    type="Positive",
+    description="Split fraction of reactant gas to be recycled from \
+                 flash drum to haber be mixed with fresh feed"
+)
+
+# ===============================================================================#
+#                              || Equations ||
+# ===============================================================================#

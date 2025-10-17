b7720794d9ce0c9723f5e1d5b2e0f9cc12d3f2e4
arkodxs <arko.a.das@gmail.com>
2025-10-17 14:56:27 +0100

    Saving crossover 1/2 code + reactor start

--- gams_temp.py
+++ gams_temp.py
@@ -5,10 +5,9 @@ from gamspy import (Container, Set, Parameter, Variable,
 m = Container()
 
 # ===============================================================================#
-#                                 || Sets ||
+#                            || Base Information ||
 # ===============================================================================#
 
-# Needs to be defined with actual data
 
 i = Set(
     Container=m,
@@ -24,42 +23,33 @@ i = Set(
     description="Involved chemical components"
 )
 
-j = Set(Container=m, name='streams', description="Flow streams in process")
+# No. of streams in process
+N_streams = 23
 
-
-# ===============================================================================#
-#                              || Parameters ||
-# ===============================================================================#
-
-# Needs to be defined with actual data
-
-v1 = Parameter(
+j = Set(
     Container=m,
-    name='v1',
-    domain=i,  # over components
-    description="Stoichiometric coefficient of component i in haber reactor"
+    name='streams',
+    records=list(range(1, N_streams + 1)),
+    description="Flow streams in process"
 )
 
 X_ure = Parameter(
     Container=m,
     name='X_Ure',
+    records=0.6,  # 60% of ammonia to urea process
     description="Split fraction of ammonia from transport to urea process"
 )
 
-# ===============================================================================#
-#                              || Variables ||
-# ===============================================================================#
 
 F = Variable(
     Container=m,
     name="F",
-    domain=[i, j],  # per comp & stream
+    domain=[j, i],  # per comp & stream
     type="Positive",
     description="Molar flowrate of component i in stream j"
 )
 
-
-X_haber = Parameter(
+X_haber = Variable(
     Container=m,
     name='X_haber',
     type="Positive",
@@ -68,5 +58,88 @@ X_haber = Parameter(
 )
 
 # ===============================================================================#
-#                              || Equations ||
+#                                  || Crossover 1 ||
 # ===============================================================================#
+
+yHfeed = Parameter(
+    Container=m,
+    name='yHfeed',
+    domain=[i],
+    records=[
+        ('H', 0.999070),
+        ('H2O', 0.000860),
+        ('O', 0.000070),
+        ('N', 0.0),
+        ('Ar', 0.0),
+        ('CO2', 0.0)
+    ],
+    description="Molar fraction of components in fresh H2 feed"
+)
+
+yNfeed = Parameter(
+    Container=m,
+    name='yHfeed',
+    domain=[i],
+    records=[
+        ('H', 0.0),
+        ('H2O', 0.000034),
+        ('O', 0.001940),
+        ('N', 0.99002),
+        ('Ar', 0.007970),
+        ('CO2', 0.000036)
+    ],
+    description="Molar fraction of components in fresh N2 feed"
+)
+
+Crossover1 = Equation(
+    Container=m,
+    name="Crossover1",
+    domain=[i],  # over components
+    description="Mass balance over crossover 1"
+)[i] = F[1, i] + F[2, i] == F[3, i]
+
+Hfeed_relation = Equation(
+    Container=m,
+    name="HFeed_relation",
+    domain=[i],  # over components
+    description="Relating flow rates of fed components in H2"
+)[i] = yHfeed['H'] * F[1, 'H'] == yHfeed[i] * F[1, i]
+
+Nfeed_relation = Equation(
+    Container=m,
+    name="NFeed_relation",
+    domain=[i],  # over components
+    description="Relating flow rates of fed components in N2"
+)[i] = yNfeed['N'] * F[2, 'N'] == yNfeed[i] * F[2, i]
+
+
+# ===============================================================================#
+#                                  || Crossover 2 ||
+# ===============================================================================#
+
+Crossover2 = Equation(
+    Container=m,
+    name="Crossover2",
+    domain=[i],  # over components
+    description="Mass balance over crossover 2"
+)[i] = F[3, i] + F[9, i] == F[4, i]
+
+
+# ===============================================================================#
+#                                  || Reactor ||
+# ===============================================================================#
+
+v1 = Parameter(
+    Container=m,
+    name='v1',
+    domain=[i],  # over components
+    records=[
+        ('H', 3),
+        ('H2O', 0),
+        ('O', 0),
+        ('N', 1),
+        ('Ar', 0),
+        ('CO2', 0)
+    ],
+    description="Stoichiometric coefficient of component i in haber reactor"
+)
